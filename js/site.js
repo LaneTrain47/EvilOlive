1d80ac3a864076c81a5880c2c19d8d35b5b72ca0
LaneTrain47 <84758670+LaneTrain47@users.noreply.github.com>
2021-07-19 15:39:41 -0400

    20210719_153900

--- js/site.js
+++ js/site.js
@@ -1,4 +1,15 @@
 function getWord() {
     let word = document.getElementById("userInput").value;
     word = word.toLowerCase().trim().replace(/[^a-zA-Z0-9]/g, "");
+
+    if (word.length < 2) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please enter a word with at least 2 letters.'
+        })
+        return;
+    }
+
+    let revWord = flipword(word);
 }
